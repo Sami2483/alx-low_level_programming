05838f39a050fa14b932521516e2e3e6d5c4bc1e
Sami2483 <samijemberu@gmail.com>
2022-06-03 14:21:30 -0700

    edit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
  * main - main block
- * Description:program that prints the lowercase alphabet 
- * in reverse
+ * Description: program that prints the lowercase alphabet in reverse
  * Return:0
  */
 int main(void)
