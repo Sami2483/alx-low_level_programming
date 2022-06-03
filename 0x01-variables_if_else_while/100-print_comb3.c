bd46972a6372c6eb13771f175058478c7e9c5b0d
Sami2483 <samijemberu@gmail.com>
2022-06-03 15:09:02 -0700

    inventing combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: program that prints all possible different combination
+ * Return:0
+ */
+int main(void)
+{
+int c;
+int x=0;
+while (x < 10)
+{
+c=0;
+while (c < 10)
+{
+if (x != c && x < c)
+{
+putchar('0' + x);
+putchar('0' + c);
+if (c+x != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
