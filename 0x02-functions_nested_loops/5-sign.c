d77e842111cf4b7ed0c1c00f97112ab6771acc2c
Sami2483 <samijemberu@gmail.com>
2022-06-07 06:52:27 -0700

    task 5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign: return 1,0,-1 if the no is +ve,0,-ve
+ * @n : the int to pint
+ * Return:0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+		return (0);
+}
