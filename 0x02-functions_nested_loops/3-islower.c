25079cad66a7ad73609d3a3ac1c94a31fb85fc03
Sami2483 <samijemberu@gmail.com>
2022-06-07 05:25:16 -0700

    task 3 first try

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+		return (0);
+}
