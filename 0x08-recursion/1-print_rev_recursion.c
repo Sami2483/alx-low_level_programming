f31ffc4b5b4579c3d43d036b216e52d1e0e3c3bc
Sami2483 <samijemberu@gmail.com>
2022-06-22 06:51:39 -0700

    taske 1;

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion: print string in reverse
- * @s: string
+ * _print_rev_recursion- print string in reverse
+ * @s: block of memory to fill
  * return: no return
  */
 void _print_rev_recursion(char *s)
 {
 	*s = '\0';
-	
 	if (*s > 0)
 	{
-		_putchar(s-1);
-		_prunt_rev_recursion(s-1);
+		_putchar(*(s - 1));
+		_prunt_rev_recursion(s - 1);
 	}
-	return;
-}
+	}
